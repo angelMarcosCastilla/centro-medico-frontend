cdaf8495efc18172c88fcede0f50127c04443cd7
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-08-28 20:22:49 -0500

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 // tailwind.config.js
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react')
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    
-    extend: {},
+    extend: {}
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      layout: {
+        disabledOpacity: '0.3', // opacity-[0.3]
+        radius: {
+          small: '2px', 
+          medium: '4px', 
+          large: '6px'
+        }
+      }
+    })
+  ]
 }
