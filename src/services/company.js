64c5c0a4b42243ed5c5eb74d32c083569819e76b
craisf <120396966+craisf@users.noreply.github.com>
2023-09-15 18:24:58 -0500

    feat : add const handleAddcompany

---
+++ src/services/company.js
@@ -0,0 +1,11 @@
+export const addCompanyService = async (data ) => {
+    const response = await fetch('http://localhost:3000/api/empresas', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),      
+    });
+    const result = await response.json();
+    return result;
+  }
