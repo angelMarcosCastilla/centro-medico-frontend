49fc97d9778a4465e1e50750e7d086a968088af9
Alexander Lurita <alex11lurita@gmail.com>
2023-09-23 12:15:49 -0500

    fix: new model template

--- src/constants/template.js
+++ src/constants/template.js
@@ -30,6 +30,41 @@ export const tableBaseTemplate = {
   rows: []
 }
 
-export const template2 = {
-  typeTemplate: 'tableWithoutHeader'
+export const columnTemplate = {
+  templateName: 'My Column Template',
+  type: 'fourColumns',
+  sections: [
+    {
+      uid: 'section1',
+      title: 'Section 1',
+      columns: [
+        { uid: 'column1', title: 'Column 1', editable: false },
+        { uid: 'column2', title: 'Column 2', editable: true },
+        { uid: 'column3', title: 'Column 3', editable: false },
+        { uid: 'column4', title: 'Column 4', editable: false }
+      ],
+      rows: [] // Array of arrays
+    }
+  ]
+}
+
+export const keyValueTemplate = {
+  templateName: 'My KeyValue Template',
+  type: 'keyValues',
+  sections: [
+    {
+      uid: 'section1',
+      title: 'Section 1',
+      items: [
+        {
+          key: { text: 'Key 1', editable: false },
+          value: { text: 'Value 1', editable: true }
+        },
+        {
+          key: { text: 'Key 2', editable: false },
+          value: { text: 'Value 2', editable: true }
+        }
+      ]
+    }
+  ]
 }
