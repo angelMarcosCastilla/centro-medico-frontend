6ed14e110654ebbd3f94e0b6cce9e43b7dbc197d
Alexander Lurita <alex11lurita@gmail.com>
2023-09-28 11:17:48 -0500

    feat: components for areas & reports table

---
+++ src/components/AttentionProcessTable.jsx
@@ -0,0 +1,309 @@
+import { useCallback, useMemo, useState } from 'react'
+import {
+  Button,
+  CardBody,
+  Chip,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  Tooltip
+} from '@nextui-org/react'
+import { ChevronDownIcon, ListTodo, SearchIcon } from 'lucide-react'
+import { toast } from 'sonner'
+import { useFetcher } from '../hook/useFetcher'
+import { changeStatus } from '../services/detalleAtencion'
+import { usePagination } from '../hook/usePagination'
+import { capitalize } from '../utils'
+import { listState, statusColorMap } from '../constants/state'
+
+const columns = [
+  { name: 'ID', uid: 'iddetatencion', sortable: true },
+  { name: 'PACIENTE', uid: 'paciente', sortable: true },
+  { name: 'CATEGORIA', uid: 'nombre_categoria', sortable: true },
+  { name: 'TIPO DE SERVICIO', uid: 'nombre_servicio', sortable: true },
+  { name: 'ESTADO', uid: 'estado', sortable: true },
+  { name: 'ACCIONES', uid: 'acciones' }
+]
+
+const INITIAL_VISIBLE_COLUMNS = [
+  'paciente',
+  'nombre_categoria',
+  'nombre_servicio',
+  'estado',
+  'acciones'
+]
+
+export default function AttentionProcessTable({ useFecherFunction }) {
+  const [filterValue, setFilterValue] = useState('')
+  const [visibleColumns, setVisibleColumns] = useState(
+    new Set(INITIAL_VISIBLE_COLUMNS)
+  )
+  const [sortDescriptor, setSortDescriptor] = useState({
+    column: 'id',
+    direction: 'ascending'
+  })
+  const { data, mutate } = useFetcher(useFecherFunction)
+
+  const hasSearchFilter = Boolean(filterValue)
+
+  const headerColumns = useMemo(() => {
+    if (visibleColumns === 'all') return columns
+
+    return columns.filter((column) =>
+      Array.from(visibleColumns).includes(column.uid)
+    )
+  }, [visibleColumns])
+
+  const filteredItems = useMemo(() => {
+    let filteredPatients = [...data]
+
+    if (hasSearchFilter) {
+      filteredPatients = filteredPatients.filter((detail) =>
+        detail.paciente.toLowerCase().includes(filterValue.toLowerCase())
+      )
+    }
+    return filteredPatients
+  }, [data, filterValue])
+
+  const {
+    items,
+    page,
+    pages,
+    rowsPerPage,
+    onNextPage,
+    onPreviousPage,
+    onRowsPerPageChange,
+    setPage
+  } = usePagination(filteredItems)
+
+  const sortedItems = useMemo(() => {
+    return [...items].sort((a, b) => {
+      const first = a[sortDescriptor.column]
+      const second = b[sortDescriptor.column]
+      const cmp = first < second ? -1 : first > second ? 1 : 0
+
+      return sortDescriptor.direction === 'descending' ? -cmp : cmp
+    })
+  }, [sortDescriptor, items])
+
+  const renderCell = useCallback((detail, columnKey) => {
+    const cellValue = detail[columnKey]
+    const estadoTexto = listState[cellValue]
+    const classChip = statusColorMap[cellValue]
+
+    switch (columnKey) {
+      case 'estado':
+        return (
+          <Chip className={`capitalize ${classChip}`} size='sm' variant='flat'>
+            {capitalize(estadoTexto)}
+          </Chip>
+        )
+      case 'acciones':
+        return (
+          <div className='relative flex items-center gap-2'>
+            <Tooltip content='Cambiar estado' color='primary'>
+              <span
+                className='text-lg text-primary-400 cursor-pointer active:opacity-50'
+                onClick={() =>
+                  handleChangeStatus(detail.iddetatencion, detail.estado)
+                }
+              >
+                <ListTodo size={20} />
+              </span>
+            </Tooltip>
+          </div>
+        )
+      default:
+        return cellValue
+    }
+  }, [])
+
+  const handleChangeStatus = async (idDetAttention, status) => {
+    const newStatus = status === 'P' ? 'A' : 'PI'
+    const result = await changeStatus(idDetAttention, newStatus)
+
+    if (result) {
+      mutate((prevData) => {
+        if (status === 'A') {
+          // Si el estado actual es "A", elimina la fila de la tabla
+          return prevData.filter(
+            (item) => item.iddetatencion !== idDetAttention
+          )
+        } else {
+          // Si el estado actual no es "A", simplemente actualiza la fila localmente
+          return prevData.map((item) => {
+            if (item.iddetatencion === idDetAttention) {
+              return { ...item, estado: newStatus }
+            }
+            return item
+          })
+        }
+      })
+    } else {
+      toast.error('Error al cambiar el estado')
+    }
+  }
+
+  const onSearchChange = useCallback((value) => {
+    if (value) {
+      setFilterValue(value)
+    } else {
+      setFilterValue('')
+    }
+  }, [])
+
+  const onClear = useCallback(() => {
+    setFilterValue('')
+  }, [])
+
+  const topContent = useMemo(() => {
+    return (
+      <div className='flex flex-col gap-4'>
+        <div className='flex justify-between gap-3 items-end'>
+          <Input
+            isClearable
+            className='w-full sm:max-w-[44%]'
+            placeholder='Buscar por nombre...'
+            startContent={<SearchIcon />}
+            value={filterValue}
+            onClear={() => onClear()}
+            onValueChange={onSearchChange}
+          />
+          <div className='flex gap-3'>
+            <Dropdown>
+              <DropdownTrigger className='hidden sm:flex'>
+                <Button
+                  endContent={<ChevronDownIcon className='text-small' />}
+                  variant='flat'
+                >
+                  Columnas
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label='Table Columns'
+                closeOnSelect={false}
+                selectedKeys={visibleColumns}
+                selectionMode='multiple'
+                onSelectionChange={setVisibleColumns}
+              >
+                {columns.map((column) => {
+                  if (column.uid === 'estado') return null
+                  return (
+                    <DropdownItem key={column.uid} className='capitalize'>
+                      {capitalize(column.name)}
+                    </DropdownItem>
+                  )
+                })}
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </div>
+        <div className='flex justify-between items-center'>
+          <span className='text-default-400 text-small'>
+            Total: {data.length} pacientes
+          </span>
+          <label className='flex items-center text-default-400 text-small'>
+            Filas por página:
+            <select
+              className='bg-transparent outline-none text-default-400 text-small'
+              value={rowsPerPage}
+              onChange={onRowsPerPageChange}
+            >
+              <option value='5'>5</option>
+              <option value='10'>10</option>
+              <option value='15'>15</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    )
+  }, [
+    filterValue,
+    visibleColumns,
+    onRowsPerPageChange,
+    data.length,
+    onSearchChange,
+    hasSearchFilter
+  ])
+
+  const bottomContent = useMemo(() => {
+    return (
+      <div className='py-2 px-2 flex justify-between items-center'>
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color='primary'
+          page={page}
+          total={pages}
+          onChange={setPage}
+        />
+        <div className='hidden sm:flex w-[30%] justify-end gap-2'>
+          <Button
+            isDisabled={pages === 1}
+            variant='flat'
+            onPress={onPreviousPage}
+          >
+            Anterior
+          </Button>
+          <Button isDisabled={pages === 1} variant='flat' onPress={onNextPage}>
+            Siguiente
+          </Button>
+        </div>
+      </div>
+    )
+  }, [items.length, page, pages, hasSearchFilter])
+
+  return (
+    <CardBody>
+      <Table
+        aria-label='Example table with custom cells, pagination and sorting'
+        isHeaderSticky
+        bottomContent={bottomContent}
+        bottomContentPlacement='outside'
+        classNames={{
+          wrapper: 'max-h-[600px]'
+        }}
+        sortDescriptor={sortDescriptor}
+        topContent={topContent}
+        topContentPlacement='outside'
+        shadow='none'
+        onSortChange={setSortDescriptor}
+      >
+        <TableHeader columns={headerColumns}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === 'actions' ? 'center' : 'start'}
+              allowsSorting={column.sortable}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          emptyContent={'No se encontraron pacientes'}
+          items={sortedItems}
+        >
+          {(item) => (
+            <TableRow key={crypto.randomUUID().toString()}>
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </CardBody>
+  )
+}
