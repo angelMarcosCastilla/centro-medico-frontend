3853bc78883ef806764bb2577a35b54294368c3c
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-10-08 13:55:24 -0500

    fix: add selectd doctor in rayox module

--- src/pages/RayosX/index.jsx
+++ src/pages/RayosX/index.jsx
@@ -1,11 +1,13 @@
 import AttentionProcessTable from '../../components/AttentionProcessTable'
 import { RAYOSX_ID } from '../../constants/areas'
 import { getInProcessAttentionsByArea } from '../../services/admission'
+import { getDoctorByAreaFunction } from '../../services/person'
 
 export default function Rayosx() {
   return (
     <AttentionProcessTable
       useFecherFunction={() => getInProcessAttentionsByArea(RAYOSX_ID)}
+      getDoctorByAreaFunction={()=>getDoctorByAreaFunction(RAYOSX_ID)}
     />
   )
 }
