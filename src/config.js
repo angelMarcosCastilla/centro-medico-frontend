bb2314d4985eb59ea3f452b91ab052e4b010a0f7
Alexander Lurita <alex11lurita@gmail.com>
2023-11-23 19:03:11 -0500

    fix: delete console log 😡

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const BASE_URL_WS =
   import.meta.env.VITE_URL_BACKEND || 'http://localhost:3000'
-console.log(BASE_URL_WS)
+
 export const redirectToResult = (id) => {
   return `${BASE_URL_WS}/api/resultados/${id}/report`
 }
